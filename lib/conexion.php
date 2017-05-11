a5bff80da3df8eb361b024a5a938d86a2ca76fa6
cetecom <cetecom@LC08L1217>
2017-05-11 13:46:40 -0300

    ultimo

---
+++ lib/conexion.php
@@ -0,0 +1,37 @@
+<?php
+
+class conexion{
+    
+    /*metodo conexion*/
+    
+   var $objconn;
+    
+   var $dbusr="root";
+   var $dbpwd="avaras08";
+   var $dbhost="localhost";
+   var $dbname ="exportaciones";
+    
+    
+    
+
+public function Conectar(){
+     $this-> objconn = new mysqli (
+             $this->dbhost,
+             $this->dbusr,
+             $this->dbpwd,
+             $this->dbname
+             );
+     if($this->objconn->connect_errno){
+         
+         return "Fallo al conectar a MySQL:(".$this->objconn->connect_errno . ")" . $this->objconn->connect_errno;
+     }
+     return true;
+     
+     
+ }
+    
+    
+    
+}
+
+            
